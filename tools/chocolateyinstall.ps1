2b45b127164bbb8a3efe1878bec3b06087526fc9
Andrei <n3rd4i@gmail.com>
2019-09-02 19:08:02 +0200

    updated to version 3.1.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,14 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-Connect-for-Desktop/3-0-0/nrfconnectsetup300ia32.exe'
+$url        = 'https://www.nordicsemi.com/-/media/Software-and-other-downloads/Desktop-software/nRF-Connect-for-Desktop/3-1-0/nrfconnectsetup310ia32.exe'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  url64bit      = $url64
   softwareName  = 'nrf-connect*'
-  checksum      = 'E06BC939E2FBA7E39E8FBF0FE7240C73DA3DBF462681D016E710E740C7AF709D'
+  checksum      = 'C02B9AF05225F6ED79B6822480423AC4104690E1C407D88E28FC93B8413CADAB'
   checksumType  = 'sha256'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
